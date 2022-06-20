f90c4ca8f85a4dfa400232bd96024c69fd5974a9
arslanov-ngc <developer.humble@gmail.com>
2022-06-20 10:44:57 +0500

    Date format changed

--- src/components/test2.jsx
+++ src/components/test2.jsx
@@ -1,11 +1,17 @@
 const Test2 = (props) => {
+  const year = props.expense.expenseDate.getFullYear(),
+    month = props.expense.expenseDate.toLocaleString("en-Us", { month: "long" }),
+    day = props.expense.expenseDate.getDate();
   return (
     <div className="Test2">
       <h4>Sarlavha: {props.expense.expenseTitle}</h4>
       <h5>Narxi: {props.expense.expenseAmount}</h5>
-      <h6>Sana: {props.expense.expenseDate.toLocaleDateString()}</h6>
+      <h6>
+        Sana: {day}-{month}, {year}
+      </h6>
     </div>
   );
 };
 
 export default Test2;
+
